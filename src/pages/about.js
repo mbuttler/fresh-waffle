037a8271c01fcc530b420f8959448cd28ca83087
Matthew Buttler <matthew.buttler@gmail.com>
2018-10-14 20:46:08 -0400

    init commit

---
+++ src/pages/about.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import Layout from '../components/layout'
+export default class About extends Component {
+  render() {
+    return (
+      <Layout>
+        <div>
+          <h1> About Us </h1>
+          <p>
+            Gastropub semiotics Wes Anderson McSweeney's DIY next level. Narwhal
+            biodiesel DIY mumblecore flannel, gluten-free pork belly trust fund
+            farm-to-table High Life keffiyeh semiotics. 8-bit artisan vegan,
+            Carles flexitarian yr food truck Helvetica skateboard ugh taxidermy
+            pop-up. +1 leggings Neutra bicycle rights readymade. Freegan four
+            dollar toast lomo ennui YOLO occupy. Butcher raw denim keytar pork
+            belly. Seitan gentrify shabby chic try-hard, cray ugh Blue Bottle
+            pop-up yr Marfa put a bird on it Vice paleo pug Brooklyn.
+          </p>
+        </div>
+      </Layout>
+    )
+  }
+}
